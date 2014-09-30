91abbf3903b2bdf9652f416a794600974816469b
TomWakefield <programming@tomwakefield.co.uk>
2014-09-30 22:34:20 +0100

    Euler0003
    Adds the solution to Euler0003

--- src/projecteuler/Caller.java
+++ src/projecteuler/Caller.java
@@ -8,7 +8,7 @@ public class Caller {
     public static void main(String[] args) {
         final long startTime = System.currentTimeMillis();
         
-        Euler0001.solve();
+        Euler0003.solve();
         
         final long endTime = System.currentTimeMillis();
         System.out.println(String.valueOf(endTime - startTime) + "ms");
---
+++ src/projecteuler/Euler0003.java
@@ -0,0 +1,27 @@
+/*
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600851475143 ?
+ */
+package projecteuler;
+
+/**
+ *
+ * @author Tom
+ */
+public class Euler0003 {
+    
+    public static void solve() {
+        long startNum = 600851475143L;
+        int factor = 3; // by inspection the number is not even
+
+        while (startNum > 1) {
+            if (startNum % factor == 0) {
+                startNum /= factor;
+            } else {
+                factor += 2; // only check odd numbers
+            }
+        }
+
+        System.out.println(factor); 
+    }
+}
