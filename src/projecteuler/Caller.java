85bafe912d8610fb214e0a0898048e33a1b47ee6
TomWakefield <programming@tomwakefield.co.uk>
2014-10-01 18:52:44 +0100

    Euler0069
    Adds the solution to Euler0069

--- src/projecteuler/Caller.java
+++ src/projecteuler/Caller.java
@@ -8,7 +8,7 @@ public class Caller {
     public static void main(String[] args) {
         final long startTime = System.currentTimeMillis();
         
-        Euler0036.solve();
+        Euler0069.solve();
         
         final long endTime = System.currentTimeMillis();
         System.out.println(String.valueOf(endTime - startTime) + "ms");
---
+++ src/projecteuler/Euler0069.java
@@ -0,0 +1,54 @@
+/*
+ * Euler0069
+ *
+ * Euler's Totient function, φ(n) [sometimes called the phi function], is used
+ * to determine the number of numbers less than n which are relatively prime to
+ * n. For example, as 1, 2, 4, 5, 7, and 8, are all less than nine and
+ * relatively prime to nine, φ(9)=6.
+ * # TABLE #
+ * It can be seen that n=6 produces a maximum n/φ(n) for n ≤ 10.
+ *
+ * Find the value of n ≤ 1,000,000 for which n/φ(n) is a maximum.
+ */
+package projecteuler;
+
+/**
+ *
+ * @author Tom
+ */
+public class Euler0069 {
+    public static boolean isPrime(int number) {
+        if (number < 2) { return false; }
+        if (number == 2) { return true; }
+        if (number % 2 == 0) { return false; }
+        
+        for (int i = 3; i <= Math.sqrt(number); i += 2) {
+            if (number % i == 0) { return false; }
+        }
+        
+        return true;
+    }
+    
+    public static int getNextPrime(int lastNumber) {
+        int nextTest = lastNumber + 1;
+        while (isPrime(nextTest) == false) {
+            nextTest++;
+        }
+        return nextTest;
+    }
+    
+    public static void solve() {
+        /*
+         * The maximum n/φ(n) requires a small φ(n) i.e. as few relatively
+         * prime numbers as possible. Therefore the target number is the product
+         * of all primes up to the point that it goes over 1000000.
+         */
+        int total = 1;
+        int nextPrime = getNextPrime(1);
+        while (total * nextPrime <= 1000000) {
+            total *= nextPrime;
+            nextPrime = getNextPrime(nextPrime);
+        }
+        System.out.println(total);
+    }
+}
