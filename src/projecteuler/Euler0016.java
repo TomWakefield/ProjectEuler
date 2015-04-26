47dc25042021f0d8c641839612c53780b9409bbb
TomWakefield <programming@tomwakefield.co.uk>
2015-04-26 14:27:01 +0100

    Euler0016
    Adds the solution to Euler0016

---
+++ src/projecteuler/Euler0016.java
@@ -0,0 +1,34 @@
+/*
+ * Project Euler 0016
+ *
+ * 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+ * What is the sum of the digits of the number 2^1000?
+ */
+package projecteuler;
+
+import java.math.BigInteger;
+
+/**
+ *
+ * @author Tom
+ */
+public class Euler0016 {
+    
+    public static void solve() {
+        BigInteger number = new BigInteger("1");
+        String strNumber;
+        int total = 0;
+        
+        for (int i = 0; i < 1000; i++) {
+            number = number.multiply(new BigInteger("2"));
+        }
+        strNumber = number.toString();
+        
+        for (int i = 0; i < strNumber.length(); i++) {
+            total += strNumber.charAt(i) - 48; // -48 converts ascii code to value
+        }
+        
+        System.out.println(String.valueOf(total));
+    }
+
+}
