fd018d1dd9703e051a8c6e86ef4a88bde7f5825e
TomWakefield <programming@tomwakefield.co.uk>
2015-04-26 13:39:52 +0100

    Euler0010
    Adds the solution to Euler0010

---
+++ src/projecteuler/Euler0010.java
@@ -0,0 +1,56 @@
+/*
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ * Find the sum of all the primes below two million.
+ */
+package projecteuler;
+
+import java.math.BigInteger;
+
+/**
+ *
+ * @author Tom
+ */
+public class Euler0010 {
+    
+    private static final int limit = 2000000;
+    private static int[] primes;
+    
+    public static void solve() {
+        fillArray(limit);
+        System.out.println(sumArray().toString());
+    }
+    
+    private static BigInteger sumArray() {
+        BigInteger total = new BigInteger("0");
+        for (int i = 0; i < primes.length; i++) {
+            if (primes[i] > 0) {
+                total = total.add(new BigInteger(String.valueOf(primes[i])));
+            }
+        }
+        return total;
+    }
+    
+    private static void fillArray(int upperLimit) {
+        int index;
+        int value;
+        
+        primes = new int[upperLimit];
+        for (int i = 1; i < upperLimit; i++) {
+            primes[i-1] = i;
+        }
+        
+        index = 0;
+        primes[0] = 0; // Set 1 to non-prime
+        
+        while (index * index < primes.length) {
+            value = primes[index];
+            if (value > 0) {
+                for (int i = value * value - 1; i < primes.length; i += value) {
+                    primes[i] = 0;
+                }
+            }
+            index++;
+        }
+    }
+
+}
